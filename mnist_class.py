97ce8b5ee59e5c15429978e780b558566d0cbb24
Koustubh2111 <koustubh.sudarshan@gmail.com>
2023-12-05 18:43:45 -0500

    CNN for data

--- mnist_class.py
+++ mnist_class.py
@@ -7,6 +7,20 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 import torchvision
-import torchvision.transforms as transforms
+#import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 from sklearn.metrics import confusion_matrix
+
+from torchvision import datasets, transforms
+
+# Define a transform to normalize the data
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                               ])
+
+# Download and load the training data
+trainset = datasets.FashionMNIST('MNIST_data/', download = True, train = True, transform = transform)
+testset = datasets.FashionMNIST('MNIST_data/', download = True, train = False, transform = transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size = 64, shuffle = True)
+testloader = torch.utils.data.DataLoader(testset, batch_size = 64, shuffle = True)
+
